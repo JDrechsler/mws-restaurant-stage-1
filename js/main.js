b2e42795f89778d001d25faa646c6252b63e6c64
Drechsler <johannes.drechsler@arvatosystems.com>
2018-05-22 14:27:35 -0400

    added support for edge and firefox

--- js/main.js
+++ js/main.js
@@ -37,7 +37,8 @@ fillNeighborhoodsHTML = (neighborhoods = self.neighborhoods) => {
 		const option = document.createElement('option');
 		option.innerHTML = neighborhood;
 		option.value = neighborhood;
-		select.append(option);
+		select.appendChild(option);
+		// select.append(option);
 	});
 }
 
@@ -64,7 +65,8 @@ fillCuisinesHTML = (cuisines = self.cuisines) => {
 		const option = document.createElement('option');
 		option.innerHTML = cuisine;
 		option.value = cuisine;
-		select.append(option);
+		select.appendChild(option);
+		// select.append(option);
 	});
 }
 
@@ -159,7 +161,7 @@ resetRestaurants = (restaurants) => {
 fillRestaurantsHTML = (restaurants = self.restaurants) => {
 	const ul = document.getElementById('restaurants-list');
 	restaurants.forEach(restaurant => {
-		ul.append(createRestaurantHTML(restaurant));
+		ul.appendChild(createRestaurantHTML(restaurant));
 	});
 	addMarkersToMap();
 }
@@ -173,26 +175,27 @@ createRestaurantHTML = (restaurant) => {
 	const image = document.createElement('img');
 	image.className = 'restaurant-img';
 	image.src = DBHelper.imageUrlForRestaurant(restaurant);
+	image.srcset
 	image.alt = DBHelper.imageAltForRestaurant(restaurant);
-	li.append(image);
+	li.appendChild(image);
 
 	const name = document.createElement('h1');
 	name.innerHTML = restaurant.name;
 	name.tabIndex = '0'
-	li.append(name);
+	li.appendChild(name);
 
 	const neighborhood = document.createElement('p');
 	neighborhood.innerHTML = restaurant.neighborhood;
-	li.append(neighborhood);
+	li.appendChild(neighborhood);
 
 	const address = document.createElement('p');
 	address.innerHTML = restaurant.address;
-	li.append(address);
+	li.appendChild(address);
 
 	const more = document.createElement('a');
 	more.innerHTML = 'View Details';
 	more.href = DBHelper.urlForRestaurant(restaurant);
-	li.append(more);
+	li.appendChild(more);
 
 	return li
 }
